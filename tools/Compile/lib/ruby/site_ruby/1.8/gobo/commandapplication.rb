36960f3e2bd62b2300e4df60ce1ee7633a189a8e
Hisham Muhammad <hisham@gobolinux.org>
2005-09-02 19:11:49 +0000

    Initial checkin.

---
+++ tools/Compile/lib/ruby/site_ruby/1.8/gobo/commandapplication.rb
@@ -0,0 +1,42 @@
+### Changelog #################################################################
+
+# 30/08/2003 - Initial Version
+
+=begin
+
+gobo/commandapplication.rb - GoboLinux CommandApplication Ruby Module
+
+Mostly stubs
+
+=end
+require 'gobo'
+
+class GoboCommandApplication < GoboApplication
+  public        # Public attributes
+  private       # Private instance/class variables
+  public        # Public methods
+    def initialize()
+      super()
+      @commands = GoboCallbacks.new
+      @descriptions = Hash.new
+    end
+
+    def addCommand(cmdName, description, aProc = nil)
+      aProc = proc {|args|
+        yield *args
+      } if aProc == nil
+      @commands.addCallback(cmdName, aProc)
+      @descriptions[cmdName] = cmdDescription
+    end
+
+    def run()
+      super()
+      command = ARGV.shift
+      if @commands[command]
+        @commands[command].call()
+      else
+        puts "#{command} not a valid command"
+      end
+    end
+  private       # Private methods
+end
