3480d5406a4d0c9be4e24aed759e7d675fd18e70
Hisham Muhammad <hisham@gobolinux.org>
2007-10-13 12:11:06 +0000

    Fix Unicode conversion of file. Fixes bug #149

--- tools/LiveCD/Data/Language/tt2_hu_HU.ts
+++ tools/LiveCD/Data/Language/tt2_hu_HU.ts
@@ -3,31 +3,31 @@
     <name>@default</name>
     <message>
         <source>Keyboard map selection</source>
-        <translation>Billentyzetkioszt� kiv�aszt�a</translation>
+        <translation>Billentyűzetkiosztás kiválasztása</translation>
     </message>
     <message>
         <source>You may select one of the following keyboard maps.</source>
-        <translation>Kiv�aszthatsz egyet a k�etkez�billentyzetkioszt�okb�.</translation>
+        <translation>Kiválaszthatsz egyet a következő billentyűzetkiosztásokból.</translation>
     </message>
     <message>
         <source>If you do not select a keyboard map, us.map</source>
-        <translation>Ha nem v�asztasz semmit, az alap�telmezett &apos;us.map&apos;</translation>
+        <translation>Ha nem választasz semmit, az alapértelmezett &apos;us.map&apos;</translation>
     </message>
     <message>
         <source>If you do not select a keyboard map, &apos;us.map&apos;</source>
-        <translation type="obsolete">Ha nem v�asztasz semmit, az alap�telmezett &apos;us.map&apos;</translation>
+        <translation>Ha nem választasz semmit, az alapértelmezett &apos;us.map&apos;</translation>
     </message>
     <message>
         <source>(the US keyboard map) is the default.  Use the UP/DOWN</source>
-        <translation>(az US billentyzetkioszt�) �v�yes. Haszn�d a FEL/LE kurzorbillentyket</translation>
+        <translation>(az US billentyűzetkiosztás) érvényes. Használd a FEL/LE kurzorbillentyűket</translation>
     </message>
     <message>
         <source>arrow keys and PageUp/PageDown to scroll through the</source>
-        <translation>� a PageUp/PageDown gombokat v�aszt�od sor� az</translation>
+        <translation>és a PageUp/PageDown gombokat választásod során az</translation>
     </message>
     <message>
         <source>whole list of choices.</source>
-        <translation>eg�z lista �tekint��ez.</translation>
+        <translation>egész lista áttekintéséhez.</translation>
     </message>
 </context>
 </TS>
